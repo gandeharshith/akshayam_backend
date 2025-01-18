bf38a421c6d4d68b29d30f7a6e211ec14da33d0b
gandeharshith <82441118+gandeharshith@users.noreply.github.com>
2025-01-18 16:31:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ app.register_blueprint(add_item_bp)
 app.register_blueprint(create_order_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True,host="0.0.0.0",port=5000)
